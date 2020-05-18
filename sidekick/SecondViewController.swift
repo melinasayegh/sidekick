88912d543a75b06f780ce635364ef0a6e0e26e6f
Melina <sayeghmelina@gmail.com>
2020-05-17 21:42:32 -0400

    Initial Commit

---
+++ sidekick/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  sidekick
+//
+//  Created by Melina Sayegh on 5/17/20.
+//  Copyright © 2020 Melina Sayegh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
