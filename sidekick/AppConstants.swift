86dfb3d20c760233412513960f1c3f0f0e39c3a7
Melina <sayeghmelina@gmail.com>
2020-05-18 20:13:21 -0400

    added color constants

--- sidekick/AppConstants.swift
+++ sidekick/AppConstants.swift
@@ -6,47 +6,28 @@
 //  Copyright © 2020 Melina Sayegh. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-struct Constants {
+struct Colors {
+    static let white = UIColor.rgba(red: 252, green: 254, blue: 255, alpha: 1)
+    static let navy = UIColor.rgba(red: 21, green: 22, blue: 65, alpha: 1)
     
-    // Colors, Images, Fonts
-    struct Design {
-        struct Color {
-            struct Primary {
-                static let white = UIColor.rgba(red: 252, green: 254, blue: 255, alpha: 1)
-                static let navy = UIColor.rgba(red: 21, green: 22, blue: 65, alpha: 1)
-               
-            }
-            struct Secondary {
-               
-            }
-            struct Grayscale {
-                
-            }
-        }
-        struct Image {
-            // example: static let icoStar = UIImage(named: "ico_imageName")
-          
-        }
-        struct Font {
-            static let body = UIFont(name: "Muli-Regular", size: 16)
-            static let logo = UIFont(name: "BalooBhai2-Medium", size: 22)
-        }
-        
-    }
-
-    struct Content {
-        
-        // example: static let Category = "category"
-    }
-
-    struct API {
-        
-        // example: static let twitterApiUrl = "https://api.twitter.com/"
-        // example: static let DB_REF = Firestore.firestore()
-        
-    }
+    static let green = UIColor.rgba(red: 115, green: 198, blue: 149, alpha: 1)
     
+    static let grey = UIColor.rgba(red: 239, green: 239, blue: 239, alpha: 1)
+    static let lightGrey = UIColor.rgba(red: 21, green: 22, blue: 65, alpha: 0.04)
+    
+    static let gradientGreen = UIColor.rgba(red: 205, green: 223, blue: 113, alpha: 1)
+    static let gradientGreenBlue = UIColor.rgba(red: 109, green: 196, blue: 151, alpha: 1)
+    static let gradientBlue = UIColor.rgba(red: 46, green: 49, blue: 146, alpha: 1)
+    
+    static let gradientPink = UIColor.rgba(red: 238, green: 42, blue: 123, alpha: 1)
+    static let gradientOrange = UIColor.rgba(red: 241, green: 93, blue: 36, alpha: 1)
+    static let gradientYellow = UIColor.rgba(red: 232, green: 230, blue: 48, alpha: 1)
+    
+}
+
+struct Fonts {
+    static let body = UIFont(name: "Muli-Regular", size: 16)
+    static let logo = UIFont(name: "BalooBhai2-Medium", size: 22)
 }
